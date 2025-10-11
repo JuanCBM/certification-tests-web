3dccfc419f05abc544417d0c8a8512bbc1cd2682
Juablaz <jc-blazquez@hotmail.com>
2025-10-11 13:28:48 +0200

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { QuizComponent } from './pages/quiz/quiz.component';
-import { ResultsComponent } from './pages/results/results.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {QuizComponent} from './pages/quiz/quiz.component';
+import {ResultsComponent} from './pages/results/results.component';
 
 export const APP_ROUTES: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'quiz', component: QuizComponent },
-  { path: 'results', component: ResultsComponent },
-  { path: '**', redirectTo: '' }
-];
+  {path: 'home', component: HomeComponent},
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {path: 'quiz', component: QuizComponent},
+  {path: 'results', component: ResultsComponent},
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }];
